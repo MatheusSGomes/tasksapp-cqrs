0ce66d5576173d28d354d6b8538aa4f56a80e83f
Matheus Gomes <math.s.gomes@gmail.com>
2024-11-13 23:37:40 -0300

    add migrations

--- Domain/Entity/User.cs
+++ Domain/Entity/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entity;
 
-[Table("tb_user")]
+[Table("Users")]
 public class User
 {
     [Key]
@@ -12,12 +12,12 @@ public class User
 
     [Required]
     [StringLength(50, MinimumLength = 3)]
-    [Column("nome", TypeName = "nvarchar(50)")]
+    // [Column("nome", TypeName = "nvarchar(50)")]
     public string? Name { get; set; }
 
     [Required]
     [StringLength(50, MinimumLength = 3)]
-    [Column("sobrenome", TypeName = "nvarchar(50")]
+    // [Column("sobrenome", TypeName = "nvarchar(50)")]
     public string? Surname { get; set; }
 
     [Required]
@@ -28,7 +28,7 @@ public class User
     public string? PasswordHash { get; set; }
 
     [Required]
-    [Column("usuario")]
+    // [Column("usuario")]
     public string? Username { get; set; }
 
     public ICollection<Workspace>? Workspaces { get; set; }
