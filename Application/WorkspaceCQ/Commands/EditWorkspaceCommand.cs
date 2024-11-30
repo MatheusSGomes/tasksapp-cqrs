82bbb24ef3d045ff5f32ffedb038510e290bf605
Matheus Gomes <math.s.gomes@gmail.com>
2024-11-30 13:22:23 -0300

    add edit workspace command and command handler

---
+++ Application/WorkspaceCQ/Commands/EditWorkspaceCommand.cs
@@ -0,0 +1,13 @@
+using Application.Response;
+using Application.WorkspaceCQ.ViewModels;
+using Domain.Enum;
+using MediatR;
+
+namespace Application.WorkspaceCQ.Commands;
+
+public record EditWorkspaceCommand : IRequest<ResponseBase<WorkspaceViewModel>>
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public StatusItemEnum? Status { get; set; }
+}
---
+++ Application/WorkspaceCQ/Handlers/EditWorkspaceCommandHandler.cs
@@ -0,0 +1,48 @@
+using Application.Response;
+using Application.WorkspaceCQ.Commands;
+using Application.WorkspaceCQ.ViewModels;
+using AutoMapper;
+using Infra.Repository.UnitOfWork;
+using MediatR;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Application.WorkspaceCQ.Handlers;
+
+public class EditWorkspaceCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<EditWorkspaceCommand, ResponseBase<WorkspaceViewModel>>
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<ResponseBase<WorkspaceViewModel>> Handle(EditWorkspaceCommand request, CancellationToken cancellationToken)
+    {
+        var workspace = await _unitOfWork.WorkspaceRepository.Get(x => x.Id == request.Id);
+
+        if (workspace is null)
+        {
+            return new ResponseBase<WorkspaceViewModel>
+            {
+                ResponseInfo = new()
+                {
+                    Title = "Workspace não encontrado",
+                    ErrorDescription = "Nenhum workspace encontrado com 'Id' informado",
+                    HttpStatus = 404
+                },
+                Value = null
+            };
+        }
+
+        if (request.Title != null)
+            workspace.Title = request.Title;
+
+        if (request.Status != null)
+            workspace.Status = request.Status;
+
+        _unitOfWork.Commit();
+
+        return new ResponseBase<WorkspaceViewModel>
+        {
+            ResponseInfo = null,
+            Value = _mapper.Map<WorkspaceViewModel>(workspace)
+        };
+    }
+}
