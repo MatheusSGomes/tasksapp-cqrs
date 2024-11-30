5f29599402187e8beef66408dc5bd8078ea5e912
Matheus Gomes <math.s.gomes@gmail.com>
2024-11-29 23:15:26 -0300

    add workspace controller

--- API/Controllers/WorkspacesController.cs
+++ API/Controllers/WorkspacesController.cs
@@ -1,4 +1,4 @@
-using Application.UserCQ.Commands;
+using Application.WorkspaceCQ.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +19,54 @@ public static class WorkspacesController
             .WithTags("Workspaces"); // Tags (agrupamento de rotas no Swagger)
 
         /* Dica: Injeção do Mediator, sempre assíncrono */
-        group.MapPost("OlaMundo", async ([FromServices] IMediator _mediator, [FromBody] CreateUserCommand command) =>
-            await _mediator.Send(command));
+        /*group.MapPost("OlaMundo", async ([FromServices] IMediator _mediator, [FromBody] CreateUserCommand command) =>
+            await _mediator.Send(command));*/
+
+        // group.MapPost("teste", () => "Meu teste");
+
+        /*
+         * Segundo parâmetro é expressão Lambda -> Tipo Delegates -> Delega uma tarefa ao sistema.
+         * Forma errada: group.MapPost("create-workspace", () => CreateWorkspace);
+         */
+
+        group.MapPost("create-workspace", CreateWorkspace)/*.RequireAuthorization()*/;
+        group.MapPut("edit-workspace", EditWorkspace);
+        group.MapDelete("delete-workspace", DeleteWorkspace);
+        group.MapGet("get-all-workspaces", GetAllWorkspaces);
+        group.MapGet("get-workspace", GetWorkspace);
+    }
+
+    public static async Task<IResult> CreateWorkspace(
+        [FromServices] IMediator _mediator,
+        [FromBody] CreateWorkspaceCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        if (result.ResponseInfo is null)
+        {
+            return Results.Ok(result.Value);
+        }
+
+        return Results.BadRequest(result.ResponseInfo);
+    }
+
+    public static async Task<IResult> EditWorkspace([FromServices] IMediator _mediator)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static async Task<IResult> DeleteWorkspace([FromServices] IMediator _mediator)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static async Task<IResult> GetAllWorkspaces([FromServices] IMediator _mediator)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static async Task<IResult> GetWorkspace([FromServices] IMediator _mediator)
+    {
+        throw new NotImplementedException();
     }
 }
